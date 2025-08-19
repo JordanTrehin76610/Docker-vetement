6ca833f9dc28b83f723001834d5424d80dbb0c48
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-08-19 16:52:43 +0200

    ptite maj 2

--- src/pages/accueil.php
+++ src/pages/accueil.php
@@ -8,7 +8,7 @@ require_once '../produit/Vetement.php'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Super Vêtement</title>
+    <title>Cebo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/style.css">
@@ -16,7 +16,8 @@ require_once '../produit/Vetement.php'
 
 
 <body>
-    <h1 class="text-center my-2"><a href="accueil.php" class="nav-link">Coucou</a></h1>
+    <?php $erreur = 0 ?>
+    <h1 class="text-center my-2"><a href="accueil.php" class="nav-link">Cebo</a></h1>
     <div class="container text-center">
         <div class="row">
             <div class="col">
@@ -39,18 +40,33 @@ require_once '../produit/Vetement.php'
     <hr>
 
     <?php
+    $allType = [
+        'T-shirt',
+        'Jean',
+        'Robe',
+        'Veste',
+        'Pull'
+    ];
+
     if (isset($_GET['type'])) { //on regarde donc si il y a un paramètre, isset permet d'esquiver l'erreur si il n'y a rien
+        $type = $_GET['type'];
+        if (!in_array($type, $allType)){
+            $erreur = 1;
+        } else {
         $trie = array_filter($Vetement, function ($item) { //On filtre pour avoir uniquement le tableau dont la clé correspond au paramètre
             return $item['type'] == $_GET['type'];
         });
-        shuffle($trie); ?>
-    <p class="w-75 mx-auto container fs-2 categorie"> <?= $_GET['type'] ?> </p>
-    <?php } else {
-        $trie = $Vetement; //Sinon rien ne change 
-        shuffle($trie); ?>
+        shuffle($trie);
+        ?><p class="w-75 mx-auto container fs-2 categorie"> <?= $_GET['type'] ?> </p>
+    <?php }
+ } else {
+        $trie = $Vetement; //Sinon rien ne change  ?>
     <p class="w-75 mx-auto container fs-2 categorie">Tous les articles</p>
-    <?php } ?>
+    <?php 
+    shuffle($trie);
+} ?>
 
+    <?php if ($erreur == 0) { ?>
     <div class="contenue mx-auto container mb-5">
         <div class="row">
             <?php foreach ($trie as $article) { ?>
@@ -73,10 +89,12 @@ require_once '../produit/Vetement.php'
                     </div>
                 </a>
             </div>
-            <?php } ?>
+            <?php }; ?>
         </div>
-
-    </div>
+    </div> <?php }
+    else { ?>
+    <p class="text-center fs-1">Marche po</p>
+    <?php } ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
     </script>
